b34f536338d77b11c009586ac18bef19a657d7b2
Loneil Stephens <loneil.stephens@gmail.com>
2020-03-15 15:58:10 -0500

    schedule appointment forms

---
+++ Ember Medical Services/ScheduleAppointment.cs
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ember_Medical_Services
+{
+    public partial class ScheduleAppointment : Form
+    {
+        public ScheduleAppointment()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ember_Medical_Services
+{
+    public partial class ScheduleAppointment : Form
+    {
+        public ScheduleAppointment()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
+>>>>>>> 03a0dbc0c7da65efd17495134287f3d6d1a56c6a
