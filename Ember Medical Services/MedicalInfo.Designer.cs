60675b25631c0933a80e9a6a2abcc2f9b454855d
Loneil Stephens <loneil.stephens@gmail.com>
2020-03-29 18:21:04 -0500

    update

--- Ember Medical Services/MedicalInfo.Designer.cs
+++ Ember Medical Services/MedicalInfo.Designer.cs
@@ -57,7 +57,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(367, 673);
+            this.button3.Location = new System.Drawing.Point(362, 673);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 20);
             this.button3.TabIndex = 65;
@@ -66,7 +66,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(465, 673);
+            this.button4.Location = new System.Drawing.Point(453, 673);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 20);
             this.button4.TabIndex = 64;
